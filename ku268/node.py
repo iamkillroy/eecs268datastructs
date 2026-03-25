75dd74054aeab715aca58c7cadfb60c18abd1eb7
iamkillroy <lucasfrias@mailfence.com>
2026-03-25 08:22:20 -0500

    Initial structure

---
+++ ku268/node.py
@@ -0,0 +1,6 @@
+
+class Node:
+    """A node is an object that stores a value and references another Node"""
+    def __init__(self, _next) -> None:
+        self._entry = None
+        self._next = None
